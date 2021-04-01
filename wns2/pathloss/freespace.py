c30f742529ba7e5f3a07a8ada2a3a5bd12b4a986
Emanuele De Santis <trunk96@gmail.com>
2021-04-01 15:40:51 +0200

    fixed free space path loss

--- wns2/pathloss/freespace.py
+++ wns2/pathloss/freespace.py
@@ -14,5 +14,5 @@ class FreeSpacePathLoss(GenericPathLoss):
         dist = math.sqrt((ue_pos[0]-bs_pos[0])**2 + (ue_pos[1]-bs_pos[1])**2 + (ue_pos[2] - bs_pos[2])**2)
         bs_frequency = bs.get_carrier_frequency()
         
-        path_loss = ((4*scipy.constants.pi*dist*bs_frequency)/scipy.constants.speed_of_light)**2
+        path_loss = 10*math.log10(((4*scipy.constants.pi*dist*bs_frequency)/scipy.constants.speed_of_light)**2)
         return path_loss
